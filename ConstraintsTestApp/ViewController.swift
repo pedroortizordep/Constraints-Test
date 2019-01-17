3af6a04cb89b7aa83316aa8ded89f7123915ecd9
Pedro Del Rio Ortiz <pedrodelrio@nbbv027843.local>
2019-01-17 10:58:46 -0200

    Initial Commit

---
+++ ConstraintsTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConstraintsTestApp
+//
+//  Created by Pedro Del Rio Ortiz on 17/01/2019.
+//  Copyright © 2019 Pedro Del Rio Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
